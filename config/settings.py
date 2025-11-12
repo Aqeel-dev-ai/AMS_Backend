8a30e4a0a9e2ed512b5a88139824eb5b23ff6db2
Aqeel-dev-ai <aqeel.nkt67@gmail.com>
2025-11-12 13:33:37 +0500

    fix : updated config for main app

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'attendance_sys_backend.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -55,7 +55,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'attendance_sys_backend.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database

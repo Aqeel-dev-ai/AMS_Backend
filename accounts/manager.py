f8ee9155756d1d7ca1fd47552cadddc146745362
mansoor-akhtar-py <mansoor.akhtar@torchsolutions.co>
2025-11-17 17:19:01 +0500

    fix :  updated user manager to use only email field and removed username field

--- accounts/manager.py
+++ accounts/manager.py
@@ -1,22 +1,20 @@
 from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, username=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email field is required")
-        if not username:
-            raise ValueError("Username field is required")
-
+            
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, username=None, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
-        return self.create_user(email=email, username=username, password=password, **extra_fields)
+        return self.create_user(email=email, password=password, **extra_fields)
